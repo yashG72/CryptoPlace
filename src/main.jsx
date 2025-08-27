7b7a3fc93ea5d54f7d81603f22ecd27e13327f8e
yashG72 <yashgoyal2874@gmail.com>
2025-08-27 23:04:41 +0530

    Refactor App component structure and styling; remove unused code and enhance layout with Navbar and Footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import CoinContextProvider from './context/CoinContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <CoinContextProvider>
     <App />
-  </StrictMode>,
+    </CoinContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
